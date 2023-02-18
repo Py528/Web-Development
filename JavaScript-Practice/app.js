2d52acd5f9bebb6d48f8a14b9b173d62cc4621e8
Py528 <pranaav.shinde5280@gmail.com>
2023-02-18 14:49:07 +0530

    Flex Box demo

---
+++ JavaScript-Practice/app.js
@@ -0,0 +1,24 @@
+// function person(Fname, Lname) {
+// 	this.fname = Fname;
+// 	this.lname = Lname;
+// }
+// const person1 = new person('pranav', 'shinde');
+// const person2 = new person('killua', 'zodyaick');
+
+// person1.getFullName = function () {
+// 	return `${this.fname} ${this.lname}`;
+// };
+
+// console.log(Array.prototype);
+
+let object = {
+	name: 'Pranav',
+	city: 'Pune',
+	getoffer: function () {
+		console.log(`${name} ${city}`);
+	},
+};
+
+let object2 = {
+	name: 'Pranaav',
+};
